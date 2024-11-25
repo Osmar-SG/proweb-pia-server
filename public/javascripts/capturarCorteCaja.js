989de4382d84b144c3a65180c1ef6f97dec331b5
Enrique Vizueth Casas <enrique.vizuethc@uanl.edu.mx>
2024-11-25 01:40:15 -0600

    Agregada la lógica de la pantalla Iniciar/Cerrar jornada

---
+++ public/javascripts/capturarCorteCaja.js
@@ -0,0 +1,74 @@
+function obtenerFechaHora() {
+    let fechaHora = new Date();
+    return fechaHora;
+}
+
+function obtenerFechaFormateada() {
+    const fechaActual = new Date();
+
+    // Obtener componentes de la fecha
+    const dia = String(fechaActual.getDate()).padStart(2, '0'); // Día (siempre dos dígitos)
+    const mes = String(fechaActual.getMonth() + 1).padStart(2, '0'); // Mes (0-11, suma 1)
+    const anio = fechaActual.getFullYear(); // Año completo
+
+    // Formatear la fecha
+    const fechaFormateada = `${dia}/${mes}/${anio}`;
+    return fechaFormateada;
+}
+
+$(document).ready(function () {
+    //Mostrar fecha de hoy
+    $("#fechaHoy").val(obtenerFechaFormateada())
+
+    // Desactivar "Cerrar Jornada" al inicio
+    $('#cerrarJornada').prop('disabled', true);
+    $('#efectivoCaja').prop('disabled', true);
+    $('#ingreso').prop('disabled', true);
+
+    // Evento para iniciar jornada
+    $('#iniciarJornada').click(function () {
+        inicioJornada = obtenerFechaHora();
+        console.log("Inicio de jornada:", inicioJornada);
+
+        // Desactivar botón "Iniciar Jornada" y activar "Cerrar Jornada"
+        $(this).prop('disabled', true);
+        $('#cerrarJornada').prop('disabled', false);
+        $('#cerrarJornada').prop('disabled', false);
+        $('#efectivoCaja').prop('disabled', false);
+        $('#ingreso').prop('disabled', false);
+    });
+
+    let efectivoCaja, ingreso, diferencia
+    // Evento para cerrar jornada
+    $('#cerrarJornada').click(function () {
+        finJornada = obtenerFechaHora();
+        console.log("Fin de jornada:", finJornada);
+
+        // Reactivar botón "Iniciar Jornada" y desactivar "Cerrar Jornada"
+        $(this).prop('disabled', true);
+        $('#iniciarJornada').prop('disabled', false);
+        $('#cerrarJornada').prop('disabled', true);
+        $('#efectivoCaja').prop('disabled', true);
+        $('#ingreso').prop('disabled', true);
+
+        efectivoCaja = $('#efectivoCaja').val()
+        ingreso = $('#ingreso').val()
+        diferencia = $('#diferencia').val()
+
+        console.log(efectivoCaja, ingreso, diferencia)
+    });
+
+    $('#efectivoCaja').on("keyup", function () {
+        let valor1 = $('#efectivoCaja').val(),
+            valor2 = $('#ingreso').val(),
+            diferencia = valor2 - valor1
+        $('#diferencia').val(diferencia)
+    })
+
+    $('#ingreso').on("keyup", function () {
+        let valor1 = $('#efectivoCaja').val(),
+            valor2 = $('#ingreso').val(),
+            diferencia = valor2 - valor1
+        $('#diferencia').val(diferencia)
+    })
+});
--- routes/corteCajas.js
+++ routes/corteCajas.js
@@ -7,7 +7,7 @@ router.get("/obtenerTodos", corteCajasFunctions.obtenerTodosCorteCaja); //obtene
 router.get("/:id", corteCajasFunctions.obtenerCorteCajaById); // buscamos el corte de caja por su id
 router.get("/usuario/:userId", corteCajasFunctions.obtenerCortePorUserId); //buscamos todos los cortes de caja por el id de usuario
 //POST Methods
-router.post("/:userId", corteCajasFunctions.crearCorteCaja);
+router.post("/:userId", corteCajasFunctions.crearCorteCaja)
 //PUT Methods
 //DELETE Methods
 
